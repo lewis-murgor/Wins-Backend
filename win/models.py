e3677d9c5f3d5a61895ce17e2dd739795f28ee06
Lewis <lewis.murgor@student.moringaschool.com>
2022-09-17 17:26:33 +0300

    define save and delete methods

--- win/models.py
+++ win/models.py
@@ -13,6 +13,15 @@ class Profile (models.Model):
     profile_photo = models.ImageField(upload_to = 'images/',blank=True)
     Bio = models.TextField(blank=True)
 
+    def __str__(self):
+        return self.Bio
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
 class Win (models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField()
@@ -22,13 +31,28 @@ class Win (models.Model):
     likes = models.ManyToManyField(User,blank=True,related_name='likes')
     comments = models.TextField()
 
+    def __str__(self):
+        return self.title
+
+    def save_win(self):
+        self.save()
+
+    def delete_win(self):
+        self.delete()
+
 class Comment(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     win = models.ForeignKey(Win,on_delete=models.CASCADE)
     comment = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.win
+
 class Like(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     win = models.ForeignKey(Win, on_delete=models.CASCADE)
     value = models.CharField(choices=LIKE_CHOICE,default='Like',max_length=10)
+
+    def __str__(self):
+        return self.win
