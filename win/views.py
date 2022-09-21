51e44340e1a82ad70e3b470bf2001f692fb3dd91
Lewis <lewis.murgor@student.moringaschool.com>
2022-09-21 20:43:24 +0300

    create registration view

--- win/views.py
+++ win/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from .models import Profile,Win,Comment,Like
+from .serializer import RegistrationSerializer
 
 # Create your views here.
+class RegistrationView(APIView):
+    def get(self, request, format=None):
+        users=User.objects.all()
+        serializers = RegistrationSerializer(users, many=True)
+        return Response(serializers.data)
