05ffe059eb6e1684edf3d6672b9207459bc9a911
Lewis <lewis.murgor@student.moringaschool.com>
2022-09-23 20:55:20 +0300

    create login url

--- win/urls.py
+++ win/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('accounts/register/', views.RegistrationView.as_view(), name='register'),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
 ]
