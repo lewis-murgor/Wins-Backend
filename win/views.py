96e9a09c926aa63e050af9bd38558f28784c0872
Lewis <lewis.murgor@student.moringaschool.com>
2022-09-23 20:55:45 +0300

    create post request

--- win/views.py
+++ win/views.py
@@ -3,8 +3,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.contrib.auth.models import User
 from .models import Profile,Win,Comment,Like
-from .serializer import RegistrationSerializer
+from .serializer import RegistrationSerializer,LoginSerializer
 from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
 
 # Create your views here.
 class RegistrationView(APIView):
@@ -26,4 +27,30 @@ class RegistrationView(APIView):
 
         else:
             data = serializer.errors
+        return Response(data)
+
+class LoginView(APIView):
+    def post(self, request, format=None):
+        serializer = LoginSerializer(data=request.data)
+        data = {}
+
+        if serializer.is_valid():
+            username = serializer.data['username']
+            password = serializer.data['password']
+
+            user = authenticate(username=username, password=password)
+            token, created = Token.objects.get_or_create(user=user)
+
+            user_manager = Token.objects.get(key=token).user.manager
+            if user_manager:
+                user_role = 'Manager'
+            else:
+                user_role = 'Employee'
+
+            data['token'] = token.key
+            data['user_role'] = user_role
+
+        else:
+            data = serializer.errors
+
         return Response(data)
