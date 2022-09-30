40780140635eaa2d377a6ded7257c5eb768ba847
Lewis <lewis.murgor@student.moringaschool.com>
2022-09-30 17:01:56 +0300

    create logout url

--- win/urls.py
+++ win/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('accounts/register/', views.RegistrationView.as_view(), name='register'),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(), name='logout'),
 ]
