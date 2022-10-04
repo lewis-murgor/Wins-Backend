999d54dce2f674af8b6d3606aa710e12183de6d7
Lewis <lewis.murgor@student.moringaschool.com>
2022-10-04 16:46:26 +0300

    create post request for win view

--- win/views.py
+++ win/views.py
@@ -76,3 +76,10 @@ class WinView(APIView):
         wins = Win.objects.all()
         serializers = WinSerializer(wins, many=True)
         return Response(serializers.data)
+
+    def post(self, request, format=None):
+        serializers = WinSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
