c9a33f8c8bd2e3201325f06222c895a73d8d729d
Lewis <lewis.murgor@student.moringaschool.com>
2022-10-05 21:06:04 +0300

    import http 404

--- win/views.py
+++ win/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
